11410fa93e93ad39a79b1ee71e8cad022e2c8625
Requaos <reqpro@requaos>
2022-06-07 02:33:08 -0400

    format stanford model outputs as list of tuple

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ category_propensity_model = pipeline("zero-shot-classification", model="facebook
 emotion_classifier = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", return_all_scores=True, device=0)
 # aggregation_strategy could be 'none' or 'simple'
 entity_recognition_model = pipeline("ner", model="xlm-roberta-large-finetuned-conll03-english", aggregation_strategy='simple', device=0)
-stanford_text_analysis = stanza.Pipeline('en', processors='tokenize,mwt,pos,ner')
+stanford_text_analysis = stanza.Pipeline('en', processors='tokenize,pos,ner')
 
 
 def label_tendencies(sequence: str, labels: list[str]) -> list[tuple]:
@@ -24,13 +24,16 @@ def recognized_entities(sequence: str):
     return entity_recognition_model(sequence)
 
 
-def stanford_stanza(sequence: str):
-    return stanford_text_analysis(sequence)
+def stanford_stanza(sequence: str) -> list[tuple]:
+    doc = stanford_text_analysis(sequence)
+    return [(word['text'], word['upos'], word['ner'], f"{word['start_char']}:{word['end_char']}")
+            for sentence in doc.sentences
+            for word in sentence.to_dict()]
 
 
 if __name__ == '__main__':
     # text = "Hello, We have tried to send you this email as HTML (pictures and words) but it wasn't possible. In order for you to see what we had hoped to show you please click here to view online in your browser:"
-    text = "Hello, My name is Sweety, and working with an Indian based app Development Company. Would you be looking for Android/IOS/ Insurance Apps/Game/Hybrid/Native Mobile APPs? And Many More Apps. If you are interested in any Mobile App . I will forward you our past work & Prices accordingly. Thanks Sweety"
+    text = "the egyptians and the greeks were of the opinion that man primarily sought experience consciously and willingly. these peoples did not have a fall in their theology as some other nations had this concept of man being driven out of paradise"
     prediction = label_tendencies(text, ['urgent', 'sensitive', 'threat', 'information', 'financial', 'problem', 'account', 'empty'])
     emotions = emotional_tendencies(text)
     print(f'Sample: {text}\n{prediction}\n{emotions}')
